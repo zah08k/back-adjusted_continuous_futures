99c1e94ee429e10b752b540f49141fe06d25c468
zah08k <100471545+zah08k@users.noreply.github.com>
2022-02-27 13:05:19 +0200

    Add files via upload

---
+++ (research-continuousdata).py
@@ -0,0 +1,102 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os.path
+
+class Backadj:
+    def __init__(self, directory, code, start):
+        self.file = open("C:/Users/Kubrat/Desktop/Python/" + directory + "/" + code + ".txt", "r")
+        self.content = self.file.read()
+        self.symbols = self.content.split(",")
+
+        self.start = start
+
+    def timeseries(self):
+        loop_1 = []
+
+        observed = 0
+        last_price = float(0)
+        last_date = None
+
+        for symbol in self.symbols:
+            if os.path.isfile("C:/Users/Kubrat/Desktop/Python/" + directory + "/" + symbol + ".csv") == True:
+                df = pd.read_csv("C:/Users/Kubrat/Desktop/Python/" + directory + "/" + symbol + ".csv",
+                                 index_col='date', parse_dates=['date'])[-self.start:]
+                df['Contract'] = symbol
+
+                if observed == 1:
+                    df = df[last_date:]
+                    adj_value = df.close[0] - last_price
+
+                    update = pd.concat(loop_1)
+                    update.open = update.open + adj_value
+                    update.high = update.high + adj_value
+                    update.low = update.low + adj_value
+                    update.close = update.close + adj_value
+
+                    last_price = df.close.iloc[-20]
+                    last_date = df.index[-20]
+                    df = df[:-20]
+
+                    del loop_1[:]
+                    loop_1.append(update)
+                    loop_1.append(df)
+
+
+                elif observed == 0:
+                    last_price = df.close.iloc[-20]
+                    last_date = df.index[-20]
+                    df = df[:-20]
+                    observed = 1
+                    loop_1.append(df)
+
+        data = pd.concat(loop_1)
+        data['openShift'] = data.open.shift(-1)
+        data.reset_index(inplace=True)
+        data['date'] = pd.to_datetime(data['date'], errors='coerce')
+        data['Month'] = data['date'].dt.month
+        data['Year'] = data['date'].dt.year
+        data = data.set_index('date')
+
+        data['Unrlzd'] = float(0)
+        data['RetDiff'] = data.openShift.diff()
+        data['Point'] = range(len(data.index))
+        data['Pnl'] = float(0)
+        data['Positive'] = 0
+        data['Negative'] = 0
+        data['Position'] = 0
+        data['DaysInpos'] = 0
+
+        years = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
+                 2022]
+
+        return data
+
+directory = 'ebm'
+code = 'ebm'
+start = 110
+
+backadj = Backadj(directory, code, start)
+df = backadj.timeseries()
+
+ranges = df.high - df.low
+z_two = ranges.std()
+print(z_two)
+
+plt.figure(figsize=(10,6))
+
+plot1, = plt.plot(ranges[-100:])
+
+plt.grid()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
